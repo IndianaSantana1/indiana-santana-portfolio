5ef7f5c33ba48789e13575a7bf7e0273cb2301fc
Foti Ceci <santana.indiana1@gmail.com>
2022-10-19 18:32:20 -0400

    update sections

--- src/components/About.js
+++ src/components/About.js
@@ -5,23 +5,23 @@ export default function About() {
     <section id="about" className="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="about-header title-font sm:text-6xl text-3xl mb-4 font-medium text-black">
+          <h1 className="about-header title-font sm:text-6xl text-3xl mb-12 font-medium text-black">
             Hi there!
             <br className="hidden lg:inline-block" />I am a creative and
-            team-oriented project manager based in New York, NY apps.
+            team-oriented project manager based in New York, NY.
           </h1>
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-black bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+              className="inline-flex text-white bg-black border-2 border-black py-2 px-6 focus:outline-none hover:border-black hover:bg-yellow-200 hover:text-black rounded text-lg"
             >
               Work With Me
             </a>
             <a
               href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-black rounded text-lg"
+              className="ml-4 inline-flex text-white border-2 border-black bg-black py-2 px-6 focus:outline-none hover:border-black hover:bg-yellow-200 hover:text-black rounded text-lg"
             >
-              See My Past Work
+              Projects
             </a>
           </div>
         </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,28 +39,25 @@ export default function Contact() {
             style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-black tracking-widest text-xs">
-                ADDRESSd
-              </h2>
-              <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
-              </p>
-              s
-            </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+          <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-3/4  px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-black tracking-widest text-xs">
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                santana.indiana1@email.com
               </a>
               <h2 className="title-font font-semibold text-black tracking-widest text-xs mt-4">
-                PHONE
+                LINKEDIN
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <a
+                className="text-indigo-400 leading-relaxed"
+                href="https://www.linkedin.com/in/indianasantana/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Indiana Santana
+              </a>
             </div>
           </div>
         </div>
@@ -71,12 +68,9 @@ export default function Contact() {
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-black sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+            Let's Connect
           </h2>
-          <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
-          </p>
+          <p className="leading-relaxed mb-5">Feel free to send me an email.</p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
